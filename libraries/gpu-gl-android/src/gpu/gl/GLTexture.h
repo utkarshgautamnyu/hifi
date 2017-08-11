e941035a8778365dbfd4abe55aae880993055d0b
cgaggar <cgaggar@climaxstudios.com>
2017-08-11 16:56:12 +0100

    more merge fixes. reverted to the master context file. commented out the isexternal() for now in gltexture.h.

--- libraries/gpu-gl-android/src/gpu/gl/GLTexture.h
+++ libraries/gpu-gl-android/src/gpu/gl/GLTexture.h
@@ -34,40 +34,44 @@ public:
         const Texture& texture = *texturePointer;
 
         // Special case external textures
-        if (texture.getUsage().isExternal()) {
-            Texture::ExternalUpdates updates = texture.getUpdates();
-            if (!updates.empty()) {
-                Texture::ExternalRecycler recycler = texture.getExternalRecycler();
-                Q_ASSERT(recycler);
-                // Discard any superfluous updates
-                while (updates.size() > 1) {
-                    const auto& update = updates.front();
-                    // Superfluous updates will never have been read, but we want to ensure the previous 
-                    // writes to them are complete before they're written again, so return them with the 
-                    // same fences they arrived with.  This can happen on any thread because no GL context 
-                    // work is involved
-                    recycler(update.first, update.second);
-                    updates.pop_front();
-                }
+		//CLIMAX_MERGE_START 
+		//Z:/HiFi_Android/HiFi_GIT/libraries/gpu-gl-android/src/gpu/gl/../gles/../gl/GLTexture.h:37:32: error: no member named 'isExternal' in 'gpu::Texture::Usage'
+		// The only instance of this being used again. replace.
+        // if (texture.getUsage().isExternal()) {
+            // Texture::ExternalUpdates updates = texture.getUpdates();
+            // if (!updates.empty()) {
+                // Texture::ExternalRecycler recycler = texture.getExternalRecycler();
+                // Q_ASSERT(recycler);
+                // // Discard any superfluous updates
+                // while (updates.size() > 1) {
+                    // const auto& update = updates.front();
+                    // // Superfluous updates will never have been read, but we want to ensure the previous 
+                    // // writes to them are complete before they're written again, so return them with the 
+                    // // same fences they arrived with.  This can happen on any thread because no GL context 
+                    // // work is involved
+                    // recycler(update.first, update.second);
+                    // updates.pop_front();
+                // }
 
-                // The last texture remaining is the one we'll use to create the GLTexture
-                const auto& update = updates.front();
-                // Check for a fence, and if it exists, inject a wait into the command stream, then destroy the fence
-                if (update.second) {
-                    GLsync fence = static_cast<GLsync>(update.second);
-                    glWaitSync(fence, 0, GL_TIMEOUT_IGNORED);
-                    glDeleteSync(fence);
-                }
+                // // The last texture remaining is the one we'll use to create the GLTexture
+                // const auto& update = updates.front();
+                // // Check for a fence, and if it exists, inject a wait into the command stream, then destroy the fence
+                // if (update.second) {
+                    // GLsync fence = static_cast<GLsync>(update.second);
+                    // glWaitSync(fence, 0, GL_TIMEOUT_IGNORED);
+                    // glDeleteSync(fence);
+                // }
 
-                // Create the new texture object (replaces any previous texture object)
-                new GLTextureType(backend.shared_from_this(), texture, update.first);
-            }
-
+                // // Create the new texture object (replaces any previous texture object)
+                // new GLTextureType(backend.shared_from_this(), texture, update.first);
+            // }
+			
+			
             // Return the texture object (if any) associated with the texture, without extensive logic
             // (external textures are 
-            return Backend::getGPUObject<GLTextureType>(texture);
-        }
-
+            //return Backend::getGPUObject<GLTextureType>(texture);
+        //}
+		//CLIMAX_MERGE_END
         if (!texture.isDefined()) {
             // NO texture definition yet so let's avoid thinking
             return nullptr;
